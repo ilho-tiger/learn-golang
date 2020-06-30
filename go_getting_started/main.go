cb4ec5c69d2f04fc4ff548ad4e224fe3d692d8b6
Ilho Song <ilho.song@siemens.com>
2020-06-30 13:11:50 -0400

    feat: declaring variables

--- go_getting_started/main.go
+++ go_getting_started/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello from a module, Gophers!")
+	// verbose way of decalaring a variable
+	var i int
+	i = 42
+	fmt.Println(i)
+
+	// little concise way than above
+	var f float32 = 3.14
+	fmt.Println(f)
+
+	// more concise way of declaring (and initializing) a variable
+	// most of time, you will use this
+	firstName := "Song"
+	fmt.Println(firstName)
+
+	b := true
+	fmt.Println(b)
 }
