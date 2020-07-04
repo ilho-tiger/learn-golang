5538575f530c17fab1800ecfce4235529828a099
Ilho Song <ilho.p.song@gmail.com>
2020-07-04 12:40:30 -0400

    feat: if and panic

---
+++ branch/if.go
@@ -0,0 +1,29 @@
+package main
+
+type User struct {
+	ID        int
+	FirstName string
+	LastName  string
+}
+
+func main() {
+	u1 := User{
+		ID:        1,
+		FirstName: "Arthur",
+		LastName:  "Dent",
+	}
+
+	u2 := User{
+		ID:        2,
+		FirstName: "Ford",
+		LastName:  "Prefect",
+	}
+
+	if u1.ID != u2.ID {
+		println("Not same user!")
+	} else if u1.FirstName == u2.FirstName {
+		println("Similar user")
+	} else {
+		println("Same user")
+	}
+}
---
+++ branch/panic.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	println("Staring application")
+
+	// do important things
+	panic("Something bad just happened")
+
+	println("Application started")
+}
